1bc60ddda2537ebbe3b84891935d5883d74610f4
vanam30 <85978070+vanam30@users.noreply.github.com>
2023-03-28 19:34:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,51 +1,61 @@
-const express = require("express");
-const collection = require("./mongo");
-const cors = require("cors");
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const express = require("express")
+const collection = require("./mongo")
+const cors = require("cors")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-app.get("/", cors(), (req, res) => {});
+app.get("/login",cors(),(req,res)=>{
 
-app.post("/", async (req, res) => {
-  const { email, password } = req.body;
+})
 
-  try {
-    const check = await collection.findOne({ email: email });
+app.post("/login",async(req,res)=>{
+    const{email,password}=req.body
 
-    if (check) {
-      res.json("exist");
-    } else {
-      res.json("notexist");
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+        }
+
     }
-  } catch (e) {
-    res.json("fail");
-  }
-});
-
-app.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-
-  const data = {
-    email: email,
-    password: password,
-  };
-
-  try {
-    const check = await collection.findOne({ email: email });
-
-    if (check) {
-      res.json("exist");
-    } else {
-      res.json("notexist");
-      await collection.insertMany([data]);
+    catch(e){
+        res.json("fail")
     }
-  } catch (e) {
-    res.json("fail");
-  }
-});
 
-app.listen(8000, () => {
-  console.log("port connected");
-});
+})
+
+app.post("/reg",async(req,res)=>{
+    const{email,password}=req.body
+
+    const data={
+        email:email,
+        password:password
+    }
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+            await collection.insertMany([data])
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+app.listen(8000,()=>{
+    console.log("port connected");
+})
